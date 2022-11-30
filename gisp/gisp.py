4f3adfe14ef50110cd59a8059cc228b76c2b8a55
yangsheng <wu0306109@gmail.com>
2022-11-30 15:49:23 +0800

    Implement `mine_subpatterns()`.

--- gisp/gisp.py
+++ gisp/gisp.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from collections import Counter, defaultdict
 from typing import Callable, Iterable, List, NamedTuple, Tuple
 
 Sequence = List[Tuple[int, List[str]]]
@@ -32,7 +32,81 @@ def mine_subpatterns(
         projected_db: List[List[Sequence]], itemize: Callable[[int], int],
         min_support: int, min_interval: int, max_interval: int,
         min_whole_interval: int, max_whole_interval: int) -> List[Pattern]:
-    pass
+    # TODO: implement fucntions in class to avoid passing so many arguments
+
+    # Count number of (itemize(interval), item) pairs in projected database
+    counter = Counter()
+    for postfixes in projected_db:
+        # for each postfixes of a corresponding sequence,
+        # get all unique (itemize(interval), item) pairs fitting the constraints
+        unique_pairs = set()
+        for postfix in postfixes:
+            origin = postfix[0][0]  # origin interval of the postfix
+            for whole_interval, items in postfix:
+                interval = whole_interval - origin
+                if (whole_interval > max_whole_interval
+                        or interval > max_interval):
+                    # because sequence is sorted, no need to continue
+                    break
+                elif (whole_interval < min_whole_interval
+                      or interval < min_interval):
+                    # skip if not fitting the constraints
+                    continue
+
+                unique_pairs.update(
+                    (itemize(whole_interval-origin), item) for item in items)
+
+        counter.update(unique_pairs)
+
+    patterns = []
+    for (interval, item), count in counter.items():
+        if count < min_support:
+            continue
+
+        patterns.append(
+            Pattern(sequence=[(interval, item), ], support=count))
+
+        # generate child progected database by
+        # the (itemize(interval), item) pair
+        child_db = []
+        for postfixes in projected_db:
+            child_postfixes = []
+            for postfix in postfixes:
+                origin = postfix[0][0]  # origin interval of the postfix
+                for i, (whole_interval, items) in enumerate(postfix):
+                    item_interval = whole_interval-origin
+                    if item_interval > max_whole_interval:
+                        # skip redundant calculation to the next postfix
+                        # when interval exceed the max whole interval
+                        break
+                    elif not (item in items and item_interval == interval):
+                        # skip to the next item if not match
+                        continue
+
+                    # generate child postfix
+                    items = items[items.index(item)+1:]
+                    child_postfix = [(whole_interval, items), *postfix[i+1:]]
+                    if items or len(child_postfix) > 1:
+                        child_postfixes.append(child_postfix)
+
+            if child_postfixes:
+                child_db.append(child_postfixes)
+
+        if not child_db or len(child_db) < min_support:
+            continue
+
+        # recursively mine child projected database
+        subpatterns = mine_subpatterns(
+            child_db, itemize=itemize, min_support=min_support,
+            min_interval=min_interval, max_interval=max_interval,
+            min_whole_interval=min_whole_interval,
+            max_whole_interval=max_whole_interval)
+
+        for pattern in subpatterns:
+            pattern.sequence.insert(0, (interval, item))
+        patterns.extend(subpatterns)
+
+    return patterns
 
 
 def mine(
