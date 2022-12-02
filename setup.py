c20cff1f603c84157ffc02d3ed40832629d38bcd
yangsheng <wu0306109@gmail.com>
2022-12-02 13:18:37 +0800

    Add set-up file.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='gisp',
+    version='0.1.0',
+    description='Generalized Interval-extended Sequential Pattern mining',
+    author='wu0306109',
+    author_email='t110598007@ntut.org.tw',
+    packages=find_packages(),
+    requires=[],
+)
