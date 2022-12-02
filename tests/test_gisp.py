6a36bbd34613fb998bf015ce07c16c3b870f13c3
yangsheng <wu0306109@gmail.com>
2022-12-02 13:10:25 +0800

    Add test cases for `transform()`.

--- tests/test_gisp.py
+++ tests/test_gisp.py
@@ -37,18 +37,98 @@ def test_transform() -> None:
 
 
 def test_transform_item_is_sorted() -> None:
-    # TODO: check if items are sorted
-    pass
+    sequences = [
+        [(0, {'a'}), (86400, {'b', 'a', 'c'}), (259200, {'c', 'a'})],
+        [(0, {'a', 'd'}), (259200, {'c'})],
+        [(0, {'e', 'f', 'a'}), (172800, {'a', 'b'})],
+    ]
+    left = gisp.transform(sequences)
+    right = [
+        [(0, ['a']), (86400, ['a', 'b', 'c']), (259200, ['a', 'c'])],
+        [(0, ['a', 'd']), (259200, ['c'])],
+        [(0, ['a', 'e', 'f']), (172800, ['a', 'b'])],
+    ]
+    assert left == right
+
+    sequences = [
+        [(0, {'a'}), (2, {'a', 'c'}), (7, {'a', 'b'}), (20, {'c', 'f'})],
+        [(0, {'a', 'd'}), (14, {'c'}), (26, {'c'})],
+        [(0, {'e', 'a', 'f'}), (6, {'b', 'd', 'a'}), (19, {'c', 'b'})],
+    ]
+    left = gisp.transform(sequences)
+    right = [
+        [(0, ['a']), (2, ['a', 'c']), (7, ['a', 'b']), (20, ['c', 'f'])],
+        [(0, ['a', 'd']), (14, ['c']), (26, ['c'])],
+        [(0, ['a', 'e', 'f']), (6, ['a', 'b', 'd']), (19, ['b', 'c'])],
+    ]
+    assert left == right
 
 
 def test_transform_sequence_is_sorted() -> None:
-    # TODO: check if sequence is sorted by occurence time
-    pass
+    sequences = [
+        [(86400, {'b', 'a', 'c'}), (259200, {'c', 'a'}), (0, {'a'})],
+        [(0, {'a', 'd'}), (259200, {'c'})],
+        [(0, {'e', 'f', 'a'}), (172800, {'a', 'b'})],
+    ]
+    left = gisp.transform(sequences)
+    right = [
+        [(0, ['a']), (86400, ['a', 'b', 'c']), (259200, ['a', 'c'])],
+        [(0, ['a', 'd']), (259200, ['c'])],
+        [(0, ['a', 'e', 'f']), (172800, ['a', 'b'])],
+    ]
+    assert left == right
+
+    sequences = [
+        [(0, {'a'}), (7, {'a', 'b'}), (2, {'a', 'c'}), (20, {'c', 'f'})],
+        [(14, {'c'}), (26, {'c'}), (0, {'a', 'd'})],
+        [(19, {'c', 'b'}), (6, {'b', 'a', 'd'}), (0, {'e', 'a', 'f'})],
+    ]
+    left = gisp.transform(sequences)
+    right = [
+        [(0, ['a']), (2, ['a', 'c']), (7, ['a', 'b']), (20, ['c', 'f'])],
+        [(0, ['a', 'd']), (14, ['c']), (26, ['c'])],
+        [(0, ['a', 'e', 'f']), (6, ['a', 'b', 'd']), (19, ['b', 'c'])],
+    ]
+    assert left == right
 
 
 def test_transform_items_in_same_time_are_merged() -> None:
-    # TODO: check items in the are merged
-    pass
+    sequences = [
+        [
+            (86400, {'b', 'a'}),
+            (86400, {'c'}),
+            (259200, {'c', 'a'}),
+            (0, {'a'}),
+        ],
+        [(0, {'d'}), (0, {'a'}), (259200, {'c'})],
+        [(0, {'e'}), (172800, {'b'}), (172800, {'a'}), (0, {'f', 'a'})],
+    ]
+    left = gisp.transform(sequences)
+    right = [
+        [(0, ['a']), (86400, ['a', 'b', 'c']), (259200, ['a', 'c'])],
+        [(0, ['a', 'd']), (259200, ['c'])],
+        [(0, ['a', 'e', 'f']), (172800, ['a', 'b'])],
+    ]
+    assert left == right
+
+    sequences = [
+        [
+            (0, {'a'}),
+            (7, {'a', 'b'}),
+            (2, {'c'}),
+            (2, {'a'}),
+            (20, {'c', 'f'}),
+        ],
+        [(0, 'a'), (14, {'c'}), (26, {'c'}), (0, {'d'})],
+        [(0, {'a', 'f'}), (19, {'c', 'b'}), (6, {'b', 'a', 'd'}), (0, {'e'})],
+    ]
+    left = gisp.transform(sequences)
+    right = [
+        [(0, ['a']), (2, ['a', 'c']), (7, ['a', 'b']), (20, ['c', 'f'])],
+        [(0, ['a', 'd']), (14, ['c']), (26, ['c'])],
+        [(0, ['a', 'e', 'f']), (6, ['a', 'b', 'd']), (19, ['b', 'c'])],
+    ]
+    assert left == right
 
 
 def test_mine_subpatterns() -> None:
